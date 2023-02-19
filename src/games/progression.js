9a6b972a64bfbc536fe07d3f6b5bbf8098f4f4fd
Ivan Alekseenko <alexeenkoivan@gmail.com>
2023-02-19 18:09:13 +0300

    Step 8

---
+++ src/games/progression.js
@@ -0,0 +1,26 @@
+import getRandomNumber from '../getRandomNumber.js';
+import gameLogic from '../index.js';
+
+const progressionLength = 10;
+const progression = () => {
+  const result = [];
+  const stepOfProgression = getRandomNumber(2, 5);
+  const beginNumber = getRandomNumber();
+  for (let i = 0; i < progressionLength; i += 1) {
+    result.push(beginNumber + stepOfProgression * i);
+  }
+  return result;
+};
+const generatedData = () => {
+  const result = progression();
+  const hiddenIndex = getRandomNumber(0, progressionLength);
+  const correctAnswer = (result[hiddenIndex]).toString();
+  result[hiddenIndex] = '..';
+  const question = result.join(' ');
+  return [question, correctAnswer];
+};
+const condition = 'What number is missing in the progression?';
+const startGame = () => {
+  gameLogic(generatedData, condition);
+};
+export default startGame;
