f49b1dbacdefe5096df93b42c63d78d0d113a8ed
Ivan Alekseenko <alexeenkoivan@gmail.com>
2023-02-24 13:13:49 +0300

    Step 9

---
+++ src/games/prime.js
@@ -0,0 +1,24 @@
+import getRandomNumber from '../getRandomNumber.js';
+import gameLogic from '../index.js';
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+const generatedData = () => {
+  const question = getRandomNumber();
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+const condition = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const startGame = () => {
+  gameLogic(generatedData, condition);
+};
+export default startGame;
